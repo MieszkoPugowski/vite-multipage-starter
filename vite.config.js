6561f5aaeda9206f8388d254d437e7f98d3713dd
Mieszko Pugowski <mpugowski@student.agh.edu.pl>
2025-06-08 15:59:19 +0200

    fix: make index.html starting site for github pages

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index/index.html'),
+        main: resolve(__dirname, 'src/index.html'),
         login: resolve(__dirname, 'src/login/index.html'),
       }
     },
